e693f661f4bbe0159e871aaa8022fc336e10f0e0
ZavAhmed <2022cs614@student.uet.edu.pk>
2024-11-08 22:32:55 +0500

    Login Window done

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
+
+namespace ProjectQMSWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == System.Windows.Input.MouseButtonState.Pressed)
+            {
+                this.DragMove(); 
+            }
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ProjectQMSWpf;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            string enteredUsername = txtUsername.Text;
+            string enteredPassword = txtPassword.Text;
+
+            string query = "SELECT COUNT(1) FROM Users WHERE Username = @username AND Password = @password";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@username", enteredUsername);
+                    command.Parameters.AddWithValue("@password", enteredPassword);
+
+                    connection.Open();
+                    int result = (int)command.ExecuteScalar();
+
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Logged in successfully!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid username or password.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE users (
+    Id INT IDENTITY(1,1) PRIMARY KEY,  
+    username VARCHAR(50) UNIQUE,        
+    password VARCHAR(100)
+);
+
+
+INSERT INTO users (username, password)
+VALUES 
+    ('munir', '123'),
+    ('sohaib', '124'),
+    ('ali', '125'),
+    ('abrar', '126'),
+    ('abbas', '127');
+
